2a12c923dacfc4f56286ae10833ca9b24762dd16
xiaoyu7016 <wangyuw@utexas.edu>
2016-02-09 23:36:44 -0600

    final_submit

---
+++ dedicate_g3.R
@@ -0,0 +1,65 @@
+
+library(lpSolveAPI)
+
+dedicate_g3 <- function(P,C,M,L)
+{
+  Mat <- matrix(0,length(L),length(P))
+  for (j in 1:length(P))
+  {
+    Maturity = M[j]
+    if (Maturity > 1) {
+      #print(Maturity)
+      Mat[1:Maturity - 1,j] <- C[j]
+    }
+    Mat[Maturity,j] <- 100 + C[j]
+    if (sum(Maturity + 1) < nrow(Mat))
+    {
+      Mat[sum(Maturity + 1):nrow(Mat),j] = 0
+    }
+  }
+  Mat[1,length(P)] = -1
+  Mat[2,length(P)] = 1
+  for (Col in length(P):ncol(Mat))
+  {
+    for (Row in 2:nrow(Mat))
+    {
+      if (Mat[Row,sum(Col - 1)] == 1)
+      {
+        Mat[Row,Col] = -1
+      }
+      if (Mat[sum(Row - 1),Col] == -1)
+      {
+        Mat[Row,Col] = 1
+      }
+    }
+  }
+  
+  
+  Proj <- make.lp(length(L),ncol(Mat))
+  for (i in 1:ncol(Mat))
+  {
+    set.column(Proj,i,Mat[,i])
+  }
+  Price <- matrix(0,length(P))
+  for (i in 1:length(P))
+  {
+    Price[i] = P[i]
+  }
+  set.objfn(Proj,Price)
+  set.constr.type(Proj,rep("=",nrow(Mat)))
+  set.rhs(Proj,L)
+  lp.control(Proj,sense = 'min')
+  sol = solve(Proj)
+  dual = get.sensitivity.rhs(Proj)
+  #print(dual)
+  
+  returns = c(Proj, dual)
+  return (returns)
+
+}
+# Test
+
+Portfolio<-dedicate_g3(c(102,99,101,98,98,104,100,101,102,94),c(5,3.5,5,3.5,4,9,6,8,9,7),c(1,2,2,3,4,5,5,6,7,8),c(12000,18000,20000,20000,16000,15000,12000,10000))
+get.objective(Portfolio[[1]])
+get.variables(Portfolio[[1]])
+
